9f7794039299261aafd82ef30923f4c19bc82b49
Marco Campos Subelza <marcoscampomc43@gmail.com>
2024-04-29 15:59:44 -0400

    Creando y Personalizando la Página 404 para Category

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,7 +1,23 @@
-export default function() {
+import { notFound } from 'next/navigation';
+
+interface Props {
+  params: {
+    id: string;
+  };
+}
+
+export default function ( { params }: Props ) {
+
+  const { id } = params;
+
+  // Conprueba que la ruta sea la correcta de lo contrario nos muestra la pagina de error (Not-found)
+  if ( id === 'kids' ) {
+    notFound();
+  }
+
   return (
     <div>
-      <h1>Categoria id</h1>
+      <h1>Categoria Page { id }</h1>
     </div>
-  )
+  );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 
 // UI
-export { TopMenu } from './ui/top-menu/TopMenu';
+export * from './ui/top-menu/TopMenu';
+export * from './ui/not-found/PageNotFound';
