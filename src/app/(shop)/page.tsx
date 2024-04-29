cf86e3bedd27fd24b3c24ebe0b98961edbe5d099
Marco Campos Subelza <marcoscampomc43@gmail.com>
2024-04-29 16:16:04 -0400

    Creando un componente para los Title

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,11 +1,14 @@
+import { Title } from '@/components';
 import { titleFont } from '@/config/fonts';
 
 export default function Home() {
   return (
-    <main className="">
-      <h1>Hola Mundo</h1>
-      <h1 className={ `${ titleFont.className } font-bold` }>Hola Mundo</h1>
-      <h1 className={ `${ titleFont.className }` }>Hola Mundo</h1>
-    </main>
+    <>
+      <Title 
+        title="Tienda"
+        subtitle="Todos los productos"
+        className="mb-2"
+      />
+    </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 
 // UI
-export * from './ui/top-menu/TopMenu';
 export * from './ui/not-found/PageNotFound';
+export * from './ui/title/Title';
+export * from './ui/top-menu/TopMenu';
