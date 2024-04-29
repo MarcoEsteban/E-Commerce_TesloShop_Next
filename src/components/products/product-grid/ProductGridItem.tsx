cf94cf45688aaf1e12fbe08d8ff36161b49a17b8
Marco Campos Subelza <marcoscampomc43@gmail.com>
2024-04-29 17:27:37 -0400

    Creando la funcionalidad para que cambie la Imagen - MouseOver

--- src/components/products/product-grid/ProductGridItem.tsx
+++ src/components/products/product-grid/ProductGridItem.tsx
@@ -1,22 +1,31 @@
+'use client'
+
 import { Product } from '@/interfaces';
 import Image from 'next/image';
 import Link from 'next/link';
+import { useState } from 'react';
 
 interface Props {
   product: Product;
 }
 
 export const ProductGridItem = ( { product }: Props ) => {
+
+  // Usando el estado para que cambie la imagen al pasar el mouse:
+  const [ displayImage, setDisplayImage ] = useState( product.images[ 0 ] );
+
   return (
     <div className="rounded-md overflow-hidden fade-in">
 
       <Link href={ `/product/${ product.slug }` }>
         <Image
-          src={ `/products/${ product.images[ 0 ] }` }
+          src={ `/products/${ displayImage }` }
           alt={ product.title }
-          className="w-full object-cover"
+          className="w-full object-cover rounded"
           width={ 500 }
           height={ 500 }
+          onMouseEnter={ () => setDisplayImage( product.images[ 1 ] ) } // Cuando entra el mouse a la Imagen.
+          onMouseLeave={ () => setDisplayImage( product.images[ 0 ] ) } // Cuando sale el mouse de la Imagen.
         />
       </Link>
 
