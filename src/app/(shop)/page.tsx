af1643f168891e5cf42b3a4d2b951df8ee24273a
Marco Campos Subelza <marcoscampomc43@gmail.com>
2024-04-29 17:16:46 -0400

    Creando los componetes para listar los productos

--- src/app/(shop)/page.tsx
+++ src/app/(shop)/page.tsx
@@ -1,14 +1,18 @@
-import { Title } from '@/components';
-import { titleFont } from '@/config/fonts';
+import { ProductGrid, Title } from '@/components';
+import { initialData } from '@/seed/seed';
+
+const products = initialData.products;
 
 export default function Home() {
   return (
     <>
-      <Title 
+      <Title
         title="Tienda"
         subtitle="Todos los productos"
         className="mb-2"
       />
+
+      <ProductGrid products={ products } />
     </>
   );
 }
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,8 @@
 
+// PRODUCTS
+export * from './products/product-grid/ProductGrid';
+export * from './products/product-grid/ProductGridItem';
+
 // UI
 export * from './ui/not-found/PageNotFound';
 export * from './ui/title/Title';
