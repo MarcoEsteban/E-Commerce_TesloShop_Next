f7d399504ea3c3c9eaa5ca74d7af112568335109
Marco Campos Subelza <marcoscampomc43@gmail.com>
2024-05-08 13:44:11 -0400

    Realizando la pantalla de Dirección del Cliente, Verificación de Compra y Orden de Pago de forma estatica

--- src/app/(shop)/checkout/page.tsx
+++ src/app/(shop)/checkout/page.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import Image from 'next/image';
 
-import { QuantitySelector, Title } from '@/components';
+import { Title } from '@/components';
 import { initialData } from '@/seed/seed';
 
 const productsInCart = [
