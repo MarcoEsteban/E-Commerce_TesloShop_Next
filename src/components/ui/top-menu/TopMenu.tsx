9709dd6e404ed72115eb89d6ef91eefdee9ead9a
Marco Campos Subelza <marcoscampomc43@gmail.com>
2024-05-08 12:14:19 -0400

    Realizando la pantalla de Dirección del Cliente, Verificación de Compra y Orden de Pago de forma estatica

--- src/components/ui/top-menu/TopMenu.tsx
+++ src/components/ui/top-menu/TopMenu.tsx
@@ -38,7 +38,7 @@ export const TopMenu = () => {
           <IoSearchOutline className="w-5 h-5" />
         </Link>
 
-        <Link href="cart" className="mx-2">
+        <Link href="/cart" className="mx-2">
           <div className="relative">
             <span className="absolute -top-2 -right-2 px-1 text-xs rounded-full font-bold bg-blue-700 text-white">3</span>
             <IoCartOutline className="w-5 h-5" />
