eb55e1228464228454dd84856b4d02ef602268d8
Marco Campos Subelza <marcoscampomc43@gmail.com>
2024-05-14 15:21:04 -0400

    Realizando el funcionamiento del Previous(<) y el Next(>) del componente de la Pagination.tsx

--- src/components/ui/pagination/Pagination.tsx
+++ src/components/ui/pagination/Pagination.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname, useSearchParams } from 'next/navigation';
 import { IoChevronBackOutline, IoChevronForwardOutline } from 'react-icons/io5';
 
 interface Props {
@@ -6,6 +9,32 @@ interface Props {
 }
 
 export const Pagination = ( { totalPages }: Props ) => {
+
+  // ? Funcionamiento de la Paginación:
+  const pathname = usePathname();                                // Ruta(URL) donde nos encontramos.
+  const searchParams = useSearchParams();                        // Parametros que recivimos de la URL.
+  const currentPage = Number( searchParams.get( 'page' ) ) ?? 1; // Número de pagina actual en la que nos encotramos.
+
+  const createPageUrl = ( pageNumber: number | string ) => {
+
+    const params = new URLSearchParams( searchParams ); // Nos permite construir nuestro parametros en la URL.
+
+    // params.toString() => Devuelve los parametros que tiene actualmente en la URL.
+    if ( pageNumber === '...' ) return `${ pathname }?${ params.toString() }`;
+
+    // Nos devuelve a la ruta actual donde nos encontramos.
+    if ( Number( pageNumber ) <= 0 ) return `${ pathname }`;
+
+    // Cuando hagamos  Next > y no haya más página nos retorna donde estamos.
+    if ( +pageNumber > totalPages ) return `${ pathname }?${ params.toString() }`;
+
+    // Crea los parametros(?page=3) para la paginas 
+    params.set( 'page', pageNumber.toString() );
+    return `${ pathname }?${ params.toString() }`
+
+  };
+
+
   return (
     <div className="flex justify-center text-center mt-10 mb-32">
       <nav aria-label="Page navigation example">
@@ -13,7 +42,8 @@ export const Pagination = ( { totalPages }: Props ) => {
         <ul className="flex list-style-none">
           <li className="page-item">
             <Link className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#">
+              href={ createPageUrl( currentPage -1 ) }
+            >
               <IoChevronBackOutline size={ 30 } />
             </Link>
           </li>
@@ -30,9 +60,18 @@ export const Pagination = ( { totalPages }: Props ) => {
             className="page-link relative block py-2 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
             href="#">3</a></li>
 
+          <li className="page-item"><a
+            className="page-link relative block py-2 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+            href="#">...</a></li>
+
+          <li className="page-item"><a
+            className="page-link relative block py-2 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
+            href="#">50</a></li>
+
           <li className="page-item">
             <a className="page-link relative block py-1.5 px-3 border-0 bg-transparent outline-none transition-all duration-300 rounded text-gray-800 hover:text-gray-800 hover:bg-gray-200 focus:shadow-none"
-              href="#">
+              href={ createPageUrl( currentPage + 1 ) }
+            >
               <IoChevronForwardOutline size={ 30 } />
             </a>
           </li>
