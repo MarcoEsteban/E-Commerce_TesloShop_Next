a716037d440a94b9945440ae28247673499f848f
Marco Campos Subelza <marcoscampomc43@gmail.com>
2024-05-21 13:29:36 -0400

    Agreganod un Mensaje de Error al no Seleccionar ninguna Talla al AddToCart.tsx

--- src/app/(shop)/product/[slug]/ui/AddToCart.tsx
+++ src/app/(shop)/product/[slug]/ui/AddToCart.tsx
@@ -12,11 +12,29 @@ export const AddToCart = ( { product }: Props ) => {
 
   const [ size, setSize ] = useState<Size | undefined>();
   const [ quantity, setQuantity ] = useState<number>( 1 );
+  const [ posted, setPosted ] = useState<boolean>( false );
+
+  const addTocart = () => {
+    setPosted( true );
+
+    if ( !size ) return;
+
+    console.log( { size, quantity } );
+  };
 
   // (size) => console.log( size ) :: Esto es igual a 'console.log' y realiza la misma funcionalidad
   // setSize                       :: Esto es igual a colocar '(size) => setSize(size)' y realiza la misma funcionalidad
   return (
     <>
+      {
+        posted && !size && (
+          // fade-in :: Add a animation al aparecer.
+          <span className="mt-2 text-red-500 text-sm font-semibold fade-in">
+            Debe de seleccionar una talla*
+          </span>
+        )
+      }
+
       {/* Selector de Tallas */ }
       <SizeSelector
         selectedSize={ size }
@@ -31,7 +49,10 @@ export const AddToCart = ( { product }: Props ) => {
       />
 
       {/* Button */ }
-      <button className="btn-primary my-5">
+      <button
+        onClick={ addTocart }
+        className="btn-primary my-5"
+      >
         Agregar al cariito
       </button>
     </>
